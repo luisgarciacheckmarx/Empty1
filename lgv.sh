ccb6a44ae0799749958d5bd6c536d66dd78f55f9
luisgarciacheckmarx <98318336+luisgarciacheckmarx@users.noreply.github.com>
2022-03-23 17:51:31 +0100

    Update lgv.sh

--- lgv.sh
+++ lgv.sh
@@ -1,2 +1,2 @@
 echo $1
-$1 ; RET=$?; echo RET is $RET; if ! [ "$RET" -eq 2 ] ; then echo ret is not 2 ; else echo ret is 2; fi
+i# $1 ; RET=$?; echo RET is $RET; if ! [ "$RET" -eq 2 ] ; then echo ret is not 2 ; else echo ret is 2; fi
